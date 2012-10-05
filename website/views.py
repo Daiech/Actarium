4b3b04b277c1cdcc95dd46501d02462866c8d8ca
Mauricio <mauricioaizaga@gmail.com>
2012-10-04 19:45:54 -0500

    Actarium Base

---
+++ website/views.py
@@ -0,0 +1,16 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+    if request.user.is_authenticated():
+        size = 100
+        email = request.user.email
+        default = "http://cms.myspacecdn.com/cms/Music%20Vertical/Common/Images/default_small.jpg"
+        gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+        gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+        ctx = {'TITLE':"Actarium",
+               'gravatar_url': gravatar_url}
+    else:
+        ctx = {'TITLE':"Actarium"}
+    return render_to_response('website/index.html', ctx, context_instance = RequestContext(request))
