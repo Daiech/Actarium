606e914f69dc395fd5bba43df57e65c209f7650d
Edwin Mesa <edwinfmesa@hotmail.com>
2012-12-13 12:45:35 -0500

    Primera implementacion modulo recuperacion de contrasena

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,12 @@ account_urls = patterns('',
     (r'^login', 'account.views.log_in'),
     (r'^logout', 'account.views.log_out'),
     (r'^PasswordChange', 'account.views.PasswordChange'),
+    #(r'^PasswordRestore', 'account.views.PasswordRestore'),
+    (r'^password/reset/$', 'account.views.password_reset2'), 
+       # {'post_reset_redirect' : '/account/password/reset/done/'}),
+    (r'^password/reset/done/$', 'account.views.password_reset_done2'),
+    (r'^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'account.views.views.password_reset_confirm2'), 
+          #  {'post_reset_redirect' : '/account/password/done/'}),
+    (r'^password/done/$', 'account.views.password_reset_complete2'),
+    
 )
