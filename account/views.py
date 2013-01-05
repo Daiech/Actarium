aa4496513d1aa10cfef61ae6161a44290ab0efb0
Mauricio <mauricioaizaga@gmail.com>
2013-01-05 01:44:29 -0500

    first-name = username on register.

--- account/views.py
+++ account/views.py
@@ -5,12 +5,13 @@ from account.forms import RegisterForm, UserForm
 from django.template import RequestContext  # para hacer funcionar {% csrf_token %}
 
 #Django Auth
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import password_reset, password_reset_done, password_reset_complete, password_reset_confirm
 from actions_log.views import saveActionLog
 from django.contrib.auth.models import User
+
 
 #------------------------------- <Normal User>---------------------------
 def newUser(request):
@@ -25,7 +26,7 @@ def newUser(request):
             formulario.save()
             user_name = formulario['username'].data
             user_id = User.objects.get(username=user_name)
-            saveActionLog(user_id,"SIGN_IN","username: %s, email: %s"%(user_name, formulario['email'].data),str(request.META['REMOTE_ADDR']))
+            saveActionLog(user_id, "SIGN_IN", "username: %s, email: %s" % (user_name, formulario['email'].data), str(request.META['REMOTE_ADDR']))
             return userLogin(request, user_name, formulario['password1'].data)
     else:
         formulario = RegisterForm()
@@ -57,7 +58,7 @@ def log_out(request):
     '''
         Finaliza una sesion activa
     '''
-    saveActionLog(request.user,"LOG_OUT","username: %s"%(request.user),request.META['REMOTE_ADDR'])  # Guarda la accion de cerrar sesion
+    saveActionLog(request.user,  "LOG_OUT", "username: %s" % (request.user), request.META['REMOTE_ADDR'])  # Guarda la accion de cerrar sesion
     logout(request)
     return HttpResponseRedirect('/')
 
@@ -67,17 +68,21 @@ def userLogin(request, user_name, password):
         Autentica a un usuario con los parametros recibidos
         actualmente solo se loguea con username, se espera autenticar con mail
     '''
+    try:
+        next = request.GET['next']
+    except Exception:
+        next = '/'
     acceso = authenticate(username=user_name, password=password)
     if acceso is not None:
         if acceso.is_active:
             login(request, acceso)
             user_id = User.objects.get(username=user_name)
-            saveActionLog(user_id,"LOG_IN","username: %s"%(user_name),request.META['REMOTE_ADDR'])  # Guarda la accion de inicar sesion
-            return HttpResponseRedirect('/#login')
+            saveActionLog(user_id, "LOG_IN", "username: %s" % (user_name), request.META['REMOTE_ADDR'])  # Guarda la accion de inicar sesion
+            return HttpResponseRedirect(next)
         else:
             return render_to_response('account/status.html', context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect('/account/login#errorlogin')
+        return HttpResponseRedirect('/account/login?next=' + next)
 
 
 #------------------------------- </Normal User>---------------------------
@@ -90,13 +95,13 @@ def myAccount(request):
         Control para usuarios logueados.
         se consultan los datos y se los envia al template para imprimirlos
     '''
-    last_data = "last=> username: %s, name: %s, last_name: %s, email %s"%(request.user.username, request.user.first_name, request.user.last_name, request.user.email)
+    last_data = "last=> username: %s, name: %s, last_name: %s, email %s" % (request.user.username, request.user.first_name, request.user.last_name, request.user.email)
     if request.method == "POST":
         form = UserForm(request.POST, instance=request.user)
-        
+
         if form.is_valid():
             form.save()
-            saveActionLog(request.user,"CHG_USDATA",last_data,request.META['REMOTE_ADDR'])  # Guarda datos de usuarios antes de modificarse
+            saveActionLog(request.user, "CHG_USDATA", last_data, request.META['REMOTE_ADDR'])  # Guarda datos de usuarios antes de modificarse
             update = True
         else:
             update = False
@@ -115,7 +120,7 @@ def PasswordChange(request):
         passForm = PasswordChangeForm(data=request.POST, user=request.user)
         if passForm.is_valid():
             passForm.save()
-            saveActionLog(request.user,"CHG_PASS","Password changed",request.META['REMOTE_ADDR'])  # Guarda datos de usuarios antes de modificarse
+            saveActionLog(request.user, "CHG_PASS", "Password changed", request.META['REMOTE_ADDR'])  # Guarda datos de usuarios antes de modificarse
             passUpdate = True
     else:
         passForm = PasswordChangeForm(user=request.user)
@@ -126,48 +131,20 @@ def PasswordChange(request):
 
 # --------------------------------</Cuenta de Usuario>----------------------
 
-# --------------------------------</Recuperacion de contrasena>----------------------
+# --------------------------------<Recuperacion de contrasena>----------------------
 
-#def PasswordRestore(request):
-#    if request.method == "POST":
-#        form = PasswordResetForm(data=request.POST)
-#        if form.is_valid():
-#            print ""
-#            #passResetForm.save()
-#            #saveActionLog(request.user,"CHG_PASS","Password changed",request.META['REMOTE_ADDR'])  # Guarda datos de usuarios antes de modificarse
-#    else:
-#        form = PasswordResetForm
-#    ctx = {"form": form}
-#    return render_to_response('account/password_reset.html', ctx, context_instance=RequestContext(request))
-#
-#def password_reset2(request, is_admin_site=False, template_name='registration/password_reset_form.html',
-#        email_template_name='registration/password_reset_email.html'):
-#    if request.method == "POST":
-#        form = PasswordResetForm(request.POST)
-#        if form.is_valid():
-#            if is_admin_site:
-#                form.save(domain_override=request.META['HTTP_HOST'])
-#            else:
-#                form.save(email_template_name=email_template_name)
-#            return HttpResponseRedirect('%sdone/' % request.path)
-#    else:
-#        form = PasswordResetForm()
-#    return render_to_response(template_name, {
-#        'form': form,
-#    }, context_instance=RequestContext(request))
 
 def password_reset2(request):
         """
         django.contrib.auth.views.password_reset view (forgotten password)
         """
         if not request.user.is_authenticated():
-                print "entro a password_reset2"
-                return password_reset(request, template_name='account/password_reset_form.html', email_template_name= 'account/password_reset_email.html', subject_template_name='account/password_reset_subject.txt', post_reset_redirect='/account/password/reset/done/')
-                
+            print "entro a password_reset2"
+            return password_reset(request, template_name='account/password_reset_form.html', email_template_name='account/password_reset_email.html', subject_template_name='account/password_reset_subject.txt', post_reset_redirect='/account/password/reset/done/')
         else:
-                print "no entro a password_reset2"
-                return HttpResponseRedirect("/account/")
-                
+            print "no entro a password_reset2"
+            return HttpResponseRedirect("/account/")
+
 
 def password_reset_done2(request):
         """
@@ -180,6 +157,7 @@ def password_reset_done2(request):
                 print "no entro a password_reset_done2"
                 return HttpResponseRedirect("/account/")
 
+
 def password_reset_confirm2(request, uidb36, token):
         """
         django.contrib.auth.views.password_reset_done - after password reset view
@@ -190,6 +168,7 @@ def password_reset_confirm2(request, uidb36, token):
         else:
                 print "no entro a password_reset_confirm2"
                 return HttpResponseRedirect("/account/")
+
 
 def password_reset_complete2(request):
         """
@@ -202,21 +181,4 @@ def password_reset_complete2(request):
                 print "no entro a password_reset_complete2"
                 return HttpResponseRedirect("/account/")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# --------------------------------</Recuperacion de contrasena>----------------------
