4168c03a3c62e79e576198d0ec092ce29ea75499
Mauricio Aizaga <mauricioaizaga@gmail.com>
2013-01-24 13:20:55 -0500

    Feed Back Funcionando.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class feedBack(models.Model):
+    type_feed = models.IntegerField(default=0, max_length=4)
+    email = models.CharField(max_length=60, null=False)
+    comment = models.TextField(blank=True)
+    date_added = models.DateTimeField(auto_now=True)
