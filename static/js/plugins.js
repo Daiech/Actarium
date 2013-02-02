1077b84e02e56fbf6cdb12f8a3d0188c2efb0ac9
Mauricio Aizaga <mauricioaizaga@gmail.com>
2013-02-02 12:32:00 -0500

    Add menu hover effect

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -42,11 +42,13 @@ function main(){
 
     //----------<On Menu hover>-------------------/
     $(".navbar-element").hover(function(){
-        $(this).children("div.navbar-item").addClass("navbar-item-hover");
-        $(this).children("div.navbar-button-propierties").css({"height":"15px"}, 100);
+            $(this).children("div.navbar-item").addClass("navbar-item-hover");
+            $(this).children("div.navbar-button-propierties").css({"height":"15px"}, 100);
     },function(){
-        $(this).children("div.navbar-item").removeClass("navbar-item-hover");
-        $(this).children("div.navbar-button-propierties").css({"height":"10px"}, 100);
+        if(!$(this).hasClass("menu-active")){
+            $(this).children("div.navbar-item").removeClass("navbar-item-hover");
+            $(this).children("div.navbar-button-propierties").css({"height":"10px"}, 100);
+        }
     });
     //----------</On Menu hover>-------------------/
 
