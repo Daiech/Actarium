709637b26b6d40c0fd80a438665ecd6861b51bce
Mauricio <mauricioaizaga@gmail.com>
2013-02-03 18:29:34 -0500

    Administración de organizaciones front-end parte1

--- asettings/views.py
+++ asettings/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 # from django.http import Http404
-from groups.models import billing, packages
+from groups.models import billing, packages, organizations, groups_pro
 # group_type, rel_user_group, minutes, invitations, minutes_type_1, minutes_type, reunions, admin_group, assistance, rel_user_minutes_assistance
 #from groups.forms import newGroupForm, newMinutesForm, newReunionForm
 #from django.contrib.auth.models import User
@@ -44,7 +44,16 @@ def settingsBilling(request):
 
 @login_required(login_url='/account/login')
 def settingsOrganizations(request):
-    ctx = {'TITLE': "Actarium by Daiech"}
+    try:
+        orgs = organizations.objects.filter(is_active=True, id_admin=request.user)
+    except Exception, e:
+        orgs = None
+        raise e
+    groups = list()
+    for org in orgs:
+        groups.append({"org": org, "groups_org_list": groups_pro.objects.filter(id_organization=org.id)})
+    print groups
+    ctx = {"organizations": groups}
     return render_to_response('asettings/settings_organization.html', ctx, context_instance=RequestContext(request))
 
 
