95d5ca0284aee812a93b8eaffd026cc738cf6341
Mauricio <mauricioaizaga@gmail.com>
2013-03-15 16:13:12 -0500

    Reunions template upload - Show the reunion with url

--- groups/forms.py
+++ groups/forms.py
@@ -1,8 +1,9 @@
 #encoding:utf-8
 from django import forms
 # from groups.models import group_type
-from groups.models import groups
+# from groups.models import groups
 from groups.validators import validate_date
+
 
 class newGroupForm(forms.Form):
     # group_type = group_type.objects.all()
@@ -26,43 +27,42 @@ class newGroupForm(forms.Form):
     # class Meta:
     #     model = groups
 
+
 class newMinutesForm3(forms.Form):
-    code = forms.CharField(label="Codigo", widget=forms.TextInput(attrs={'placeholder': 'Codigo de acta','autofocus': 'autofocus'}))
+    code = forms.CharField(label="Codigo", widget=forms.TextInput(attrs={'placeholder': 'Codigo de acta', 'autofocus': 'autofocus'}))
     date_start = forms.DateTimeField(label="Fecha inicio", widget=forms.widgets.DateTimeInput(attrs={'class': 'date-pick'}), input_formats=['%Y-%m-%d %I:%M %p'])
     agenda = forms.CharField(label="Orden del día", widget=forms.Textarea(attrs={'placeholder': 'Orden del día'}))
     agreement = forms.CharField(label="Acuerdos", widget=forms.Textarea(attrs={'placeholder': 'Acuerdos'}))
-    
+
 
 class newMinutesForm(forms.Form):
-    TYPES = [('ORDINARIA','ORDINARIA'),('EXTRAORDINARIA','EXTRAORDINARIA')]
-    code = forms.CharField(label="Codigo", widget=forms.TextInput(attrs={'placeholder': 'Codigo de acta','autofocus': 'autofocus'}))
+    TYPES = [('ORDINARIA', 'ORDINARIA'), ('EXTRAORDINARIA', 'EXTRAORDINARIA')]
+    code = forms.CharField(label="Codigo", widget=forms.TextInput(attrs={'placeholder': 'Codigo de acta', 'autofocus': 'autofocus'}))
     date_start = forms.DateTimeField(label="Fecha inicio", widget=forms.widgets.DateTimeInput(attrs={'class': 'date-pick'}), input_formats=['%Y-%m-%d %I:%M %p'])
     location = forms.CharField(label="Lugar", widget=forms.TextInput(attrs={'placeholder': 'Lugar'}))
     agenda = forms.CharField(label="Orden del día", widget=forms.Textarea(attrs={'placeholder': 'Orden del día'}))
     agreement = forms.CharField(label="Acuerdos", widget=forms.Textarea(attrs={'placeholder': 'Acuerdos'}))
     date_end = forms.DateTimeField(label="Fecha finalización", widget=forms.widgets.DateTimeInput(attrs={'class': 'date-pick'}), input_formats=['%Y-%m-%d %I:%M %p'])
-    type_reunion = forms.ChoiceField( widget=forms.Select(), choices=TYPES)
-
+    type_reunion = forms.ChoiceField(widget=forms.Select(), choices=TYPES)
 
 
 class newReunionForm(forms.Form):
     #formulario para agregar una nueva reunion
-    date_reunion = forms.DateTimeField(label="Fecha", validators=[validate_date], widget=forms.widgets.DateTimeInput(attrs={'class': 'date-pick'}), input_formats=['%Y-%m-%d %I:%M %p'])
+    date_reunion = forms.DateTimeField(label="Fecha", validators=[validate_date], widget=forms.widgets.DateTimeInput(attrs={'class': 'date-pick', 'placeholder': 'Ej: 2013-03-15 02:52 pm'}), input_formats=['%Y-%m-%d %I:%M %p'])
     title = forms.CharField(label="Título", widget=forms.TextInput(attrs={'placeholder': 'Título'}))
     locale = forms.CharField(label="Lugar", widget=forms.TextInput(attrs={'placeholder': 'Lugar'}))
-    agenda = forms.CharField(label="Orden del día", widget=forms.Textarea(attrs={'placeholder': 'Objetivos de la reunión'}))
-
+    agenda = forms.CharField(label="Descripción", widget=forms.Textarea(attrs={'placeholder': 'Objetivos de la reunión'}))
 
 
 class newOrganizationForm(forms.Form):
     name = forms.CharField(label="Nombre de Organización", widget=forms.TextInput(attrs={'placeholder': 'Nombre de organización', 'autofocus': 'autofocus'}))
     logo_address = forms.FileField(label="Logo", required=False, widget=forms.FileInput(attrs={'placeholder': 'URL del logo'}))
-    description = forms.CharField(label="Descripción", required=False, widget=forms.TextInput(attrs={'class': '', 'placeholder': 'Nombre de organización'}))
+    description = forms.CharField(label="Descripción", required=False, widget=forms.TextInput(attrs={'class': '', 'placeholder': 'Descripción'}))
 
     # def save(self):
     #     user = super(newOrganizationForm, self).save()
     #     return user
 
+
 class uploadMinutesForm(forms.Form):
-    minutesFile = forms.FileField(label="Archivos", widget=forms.FileInput(attrs={'multiple':'multiple','accept':'application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/pdf'}))
-    
+    minutesFile = forms.FileField(label="Archivos", widget=forms.FileInput(attrs={'multiple': 'multiple', 'accept': 'application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/pdf'}))
