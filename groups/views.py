7c9336544071973e07e9deddcc9ef0bb5ce34515
edwinfmesa <edwinfmesa@hotmail.com>
2013-06-21 14:19:41 -0500

    FIX - name of template - adminDNIGroup.html

--- groups/views.py
+++ groups/views.py
@@ -240,7 +240,7 @@ def groupDNISettings(request, slug_group):
             users_dni.append(m.id_user)      
         members = rel_user_group.objects.filter(id_group = g, is_member = True, is_active=True).exclude(id_user__in =users_dni)
         ctx = {"group": g,"is_admin": _user_rel.is_admin, 'members':members,'members_dni':members_dni}
-        return render_to_response('groups/adminDNIgroup.html', ctx, context_instance=RequestContext(request))
+        return render_to_response('groups/adminDNIGroup.html', ctx, context_instance=RequestContext(request))
     except groups.DoesNotExist:
         return HttpResponseRedirect('/groups/')
 
