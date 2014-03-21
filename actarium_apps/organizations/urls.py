255f1699a5ee322867fe6b0f15cd53b913b55050
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-03-20 19:02:39 -0500

    Icons on team template

--- actarium_apps/organizations/urls.py
+++ actarium_apps/organizations/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, patterns
 
 urlpatterns = patterns('actarium_apps.organizations.views',
-    url(r'^$', 'readOrg', name="show_org"),
+    url(r'^$', 'readOrg', name="show_orgs"),
     url(r'^create/', 'createOrg', name="create_org"),
     url(r'^(?P<slug_org>[-\w]+)/$', 'readOrg', name="show_org"),
     url(r'^(?P<slug_org>[-\w]+)/edit$', 'updateOrg', name="update_org"),
