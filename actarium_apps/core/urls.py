e2ed697dd4f3c1acfb494f1749eed9ffd1aa0251
Edwin Mesa <edwinfmesa@hotmail.com>
2014-03-21 16:42:04 -0500

    Add services info for organizations

--- actarium_apps/core/urls.py
+++ actarium_apps/core/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url, patterns
 from .views import *
 
 customers_services = patterns('',
-    url(r'^servicios$', read_organizations_services, name='read_organizations_services'),
     url(r'^servicios/(?P<slug_org>[-\w]+)/$', read_organizations_services, name='read_organization_services'),
 )
 
--- actarium_apps/core/views.py
+++ actarium_apps/core/views.py
@@ -1,13 +1,14 @@
 #encoding:utf-8
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 
 @login_required()
-def read_organizations_services(request,slug_org=False):
+def read_organizations_services(request,slug_org):
     if slug_org:
-        my_orgs = [request.user.organizationsuser_user.get_org(slug=slug_org)]
-        show_all = True
-    else:
-        my_orgs = request.user.organizationsuser_user.get_orgs_by_role_code("is_creator")
-    return render(request,'organizations_services.html', locals())
+        org = request.user.organizationsuser_user.get_org(slug=slug_org)
+        if org and org.has_user_role(request.user,'is_creator'):
+            is_creator = True
+        return render(request,'organizations_services.html', locals())   
+    raise Http404
