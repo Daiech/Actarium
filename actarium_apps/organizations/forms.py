41a15fd6bf4a45d9410376859c0e3615dfbb1b9a
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-03-26 16:20:38 -0500

    front-end updated

--- actarium_apps/organizations/forms.py
+++ actarium_apps/organizations/forms.py
@@ -1,12 +1,13 @@
 #encoding:utf-8
 from django import forms
 from .models import Organizations
+from django.utils.translation import ugettext_lazy as _
 
 
 class OrganizationForm(forms.ModelForm):
     name = forms.CharField(label="Nombre de Organización", widget=forms.TextInput(attrs={'placeholder': 'Nombre de organización', 'autofocus': 'autofocus'}))
-    description = forms.CharField(label="Descripción (opcional)", required=False, widget=forms.TextInput(attrs={'class': '', 'placeholder': 'Descripción'}))
-    image_path = forms.FileField(label="Logo (opcional)", required=False, widget=forms.FileInput(attrs={'placeholder': 'URL del logo'}))
+    description = forms.CharField(label="Descripción (opcional)", max_length=100, help_text=_(u'Máx. 100 caractéres.'), required=False, widget=forms.TextInput(attrs={'class': '', 'placeholder': 'Descripción'}))
+    image_path = forms.FileField(label="Logo (opcional)", required=False, widget=forms.FileInput(attrs={'placeholder': _('URL del logo')}), help_text=_("No seleccionar para conservar el actual"))
 
     class Meta:
         model = Organizations
