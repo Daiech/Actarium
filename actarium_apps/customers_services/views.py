62a44ba750f0c145b8eb19dc75fb903fed32c799
Edwin Mesa <edwinfmesa@hotmail.com>
2014-03-27 15:15:20 -0500

    Show packages list

--- actarium_apps/customers_services/views.py
+++ actarium_apps/customers_services/views.py
@@ -10,6 +10,15 @@ def read_pricing(request, slug_org):
         org = request.user.organizationsuser_user.get_org(slug=slug_org)
         if org and org.has_user_role(request.user,'is_creator'):
             is_creator = True
+
+        S000 = Services.objects.get_or_none(code='S000')
+        S001 = Services.objects.get_or_none(code='S001')
+        S002 = Services.objects.get_or_none(code='S002')
+        S003 = Services.objects.get_or_none(code='S003')
+        S004 = Services.objects.get_or_none(code='S004')
+        S005 = Services.objects.get_or_none(code='S005')
+        S006 = Services.objects.get_or_none(code='S006')
+
         services_categories = ServicesCategories.objects.filter(is_active=True)
         return render(request,'pricing.html', locals())
     raise Http404
