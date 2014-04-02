bd14fa4439b30d070c9754c12d9f14652dbbb344
Edwin Mesa <edwinfmesa@hotmail.com>
2014-04-02 18:34:27 -0500

    Show orders status for organizations services

--- actarium_apps/core/views.py
+++ actarium_apps/core/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
+from actarium_apps.customers_services.models import OrderItems
+
 
 @login_required()
 def read_organizations_services(request,slug_org):
     if slug_org:
         org = request.user.organizationsuser_user.get_org(slug=slug_org)
-        if org and org.has_user_role(request.user,'is_creator'):
-            is_creator = True
+        if org and org.has_user_role(request.user,'is_admin'):
+            is_admin = True
+            customer_service = org.organizationservices_organization.get_members_service_active()
+            customer = request.user.actariumcustomers_user.all()[0].customer
+            order_items = OrderItems.objects.filter(customer_service=customer_service, order__customer=customer)
         return render(request,'organizations_services.html', locals())   
     raise Http404
 
