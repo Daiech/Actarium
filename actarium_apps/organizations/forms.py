55955c1f8b329b92caa5c5ca012e4f8d9c4d6ac2
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-04-08 17:32:11 -0500

    i18n in forms

--- actarium_apps/organizations/forms.py
+++ actarium_apps/organizations/forms.py
@@ -7,9 +7,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class OrganizationForm(forms.ModelForm):
-    name = forms.CharField(label="Nombre de Organización",max_length=100,  widget=forms.TextInput(attrs={'placeholder': 'Nombre de organización', 'autofocus': 'autofocus'}))
-    description = forms.CharField(label="Descripción (opcional)", max_length=100, help_text=_(u'Máx. 100 caractéres.'), required=False, widget=forms.TextInput(attrs={'class': '', 'placeholder': 'Descripción'}))
-    image_path = forms.FileField(label="Logo (opcional)", required=False, widget=forms.FileInput(attrs={'placeholder': _('URL del logo')}), help_text=_("No seleccionar para conservar el actual"))
+    name = forms.CharField(label=_(u"Nombre de Organización"),max_length=100,  widget=forms.TextInput(attrs={'placeholder': _(u'Nombre de organización'), 'autofocus': 'autofocus'}))
+    description = forms.CharField(label=_(u"Descripción (opcional)"), max_length=100, help_text=_(u'Máx. 100 caractéres.'), required=False, widget=forms.TextInput(attrs={'class': '', 'placeholder': 'Descripción'}))
+    image_path = forms.FileField(label=_(u"Logo (opcional)"), required=False, widget=forms.FileInput(attrs={'placeholder': _('URL del logo')}), help_text=_("No seleccionar para conservar el actual"))
 
     def clean_name(self):
     	name = self.cleaned_data.get('name')
