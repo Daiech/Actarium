2fa8becf2f18c26f338207fccc8b5cd18c3ff137
edwinfmesa <edwinfmesa@hotmail.com>
2014-04-21 10:09:32 -0500

    FIX mongodb configuration with security and ADD documentation

--- apps/actions_log/utils.py
+++ apps/actions_log/utils.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 import datetime
+from pymongo import MongoClient
 
 def saveErrorLog(errordata):
     try:
@@ -12,23 +13,17 @@ def saveErrorLog(errordata):
         pass
 
 def connect_to_actarium_db():
-	MONGODB = settings.MONGODB
-	try:
-		connection = MongoClient('localhost', MONGODB['PORT'])
-	except:
-		mongodb_error =  'problem connecting to mongo client on port: %s'%(MONGODB['PORT'])
-		saveErrorLog(mongodb_error)
-		print mongodb_error
-		return False
+    MONGODB = settings.MONGODB
+    
+    uri = "localhost:27017/actarium"
+    try:
+        connection = MongoClient('mongodb://admin:123456@localhost/admin')
+        print "----conected to localhost"
+    except:
+        error = "Error connecting to MongoDB"
+        print error
+        saveErrorLog(mongodb_error)
+    
+    actarium_db = connection.actarium
 
-	try:
-		response = connection.actarium(MONGODB['USER'], MONGODB['PASSWORD'])
-	except:
-		mongodb_error =  'Error authenticating mongodb user %s, %s '%(MONGODB['USER'], MONGODB['PASSWORD'])
-		saveErrorLog(mongodb_error)
-		print mongodb_error
-		return False
-
-	actarium_db = connection.actarium
-
-	return actarium_db
+    return actarium_db
