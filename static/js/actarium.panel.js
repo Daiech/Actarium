cbf47b5b4591fe70c5b3a3190cfbb2b9fe8e6287
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-07-01 11:04:38 -0500

    Change panels and view actives

--- static/js/actarium.panel.js
+++ static/js/actarium.panel.js
@@ -13,7 +13,7 @@ $(document).on("click", ".close-panel", closePanel);
 function loadPanelContent(html_content){
 	$("#activity-content").html(html_content);
 }
-function loadPanel (a, callback) {
+function loadPanel (a) {
 	ctx = a;
 	$("#minutesPanel").html(swig.render($("#minutesPanelTpl").html(), {locals: ctx})).removeClass("hidden");
 	$(".btn-pull-right").addClass("btn-pull-right-with-panel");
@@ -23,7 +23,9 @@ function loadPanel (a, callback) {
 	$("#activity-content").html("loading...")
 		.css("height", ($("#activity").height() - 70) + "px")
 		.niceScroll();
-	callback();
+	$("#dropdownPanelList i.glyphicon").removeClass("glyphicon-ok");
+	$("#" + ctx.id).find("i.glyphicon").addClass("glyphicon-ok");
+	ctx.callback();
 }
 function closePanel (e) {
 	e.preventDefault();
@@ -31,4 +33,6 @@ function closePanel (e) {
 	$("#activity").addClass("hidden");
 	$("#minutes-processor-container").removeClass("mpc");
 	$(".btn-pull-right").removeClass("btn-pull-right-with-panel");
+	var id =  $(this).closest(".actarium-panel").attr("id").substring("panel_".length);
+	$("#" + id).find("i.glyphicon").removeClass("glyphicon-ok");
 }
