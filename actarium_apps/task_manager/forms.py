5ae3bf1e1a5ac4a60c9aaedca6de505f5ad6b875
Edwin Mesa <edwinfmesa@hotmail.com>
2014-07-04 13:26:33 -0500

    Add task_manager form

---
+++ actarium_apps/task_manager/forms.py
@@ -0,0 +1,10 @@
+#encoding:utf-8
+from django import forms
+from apps.groups_app.validators import validate_date
+from django.utils.translation import ugettext_lazy as _
+
+class newMinutesForm3(forms.Form):
+    name = forms.CharField(required=True)
+    description = forms.CharField(required=False)
+    responsible = forms.CharField()
+    due = forms.DateTimeField(input_formats=['%Y-%m-%d'])
