a17345e4ff2e683bafa7d3f1ae0a2a9c64baae39
Edwin Mesa <edwinfmesa@hotmail.com>
2014-07-18 12:38:48 -0500

    new styles for task_managers

--- actarium_apps/task_manager/utils.py
+++ actarium_apps/task_manager/utils.py
@@ -10,9 +10,11 @@ def task_as_json(task_obj):
                 "short_title":task_obj.name[0:title_max_length]+"..." if len(task_obj.name) >= title_max_length else task_obj.name,
                 "description":task_obj.description,
                 "short_description":task_obj.description[0:description_max_length]+"..." if len(task_obj.description) >= description_max_length else task_obj.description,
-                "img":showgravatar(task_obj.responsible.email,25), 
+                "responsible_img":showgravatar(task_obj.responsible.email,25), 
                 "responsible":task_obj.responsible.first_name, 
                 "responsible_id":task_obj.responsible.id, 
+                "creator":task_obj.creator.first_name, 
+                "creator_img":showgravatar(task_obj.creator.email,25),
                 "color":task_obj.color,
                 "status":task_obj.status,
                 "due":task_obj.due.strftime('%Y-%m-%d') if task_obj.due else ""
