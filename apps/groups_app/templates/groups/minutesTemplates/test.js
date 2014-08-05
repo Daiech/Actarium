016d3d16c4752881c054bcd2d89e24a2c907bbd0
Edwin Mesa <edwinfmesa@hotmail.com>
2014-08-05 16:25:53 -0500

    front-end for new minutes templates

--- apps/groups_app/templates/groups/minutesTemplates/test.js
+++ apps/groups_app/templates/groups/minutesTemplates/test.js
@@ -1,6 +1,8 @@
 function sendForm(e){
 	e.preventDefault();
 	e.stopPropagation();
+	tinyMCE.triggerSave();
+	$.each($("#minutes textarea"),function(index,value){$(this).val($(this).val().replace(/\ /g, '&nbsp;'))});
  	console.log($(this).serialize())
 }
 
@@ -9,13 +11,17 @@ function submitForm(){
 }
 
 
+setForm = function(){
+   serialized = $("#form-new-minutes").serialize();
+   $.each(serialized.split('&'), function (index, elem) {
+	   var vals = elem.split('=');
+	   $("[name='" + vals[0] + "']").val(vals[1]);
+	});
+   $.each($("#minutes textarea"),function(index,value){console.log(index);tinyMCE.activeEditor.setContent(tinyMCE.DOM.decode(unescape($(value).val())))});
+}
+
+
 $(document).on("submit","#form-new-minutes", sendForm)
 $(document).on("click","#sendForm", submitForm)
 
 
-function readForm(){$.each(serialized.split('&'), function (index, elem) {
-   var vals = elem.split('=');
-   $("[name='" + vals[0] + "']").val(vals[1]);
-});}
-
-tinyMCE.triggerSave();
