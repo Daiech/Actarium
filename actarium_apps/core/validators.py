66faab8a0f1efdf86d1578d11aa90df3291f61f4
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-09-02 17:47:13 -0500

    Change min numbers of members to buy in org

--- actarium_apps/core/validators.py
+++ actarium_apps/core/validators.py
@@ -6,9 +6,9 @@ def greater_than_or_equal_to_five(value):
 	try:
 		value = int(value)
 	except:
-		raise ValidationError(_("Ingrese un numero entero."))
-	if value < 5:
-		raise ValidationError(_("El numero debe ser mayor o igual a 5"))
+		raise ValidationError(_(u"Ingrese un número entero."))
+	if value < 1:
+		raise ValidationError(_(u"El número debe ser mayor o igual a 5"))
 	else:
 		print "Correcto"
 
