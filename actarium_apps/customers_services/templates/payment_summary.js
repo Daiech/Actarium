6b7f65d4561cfcd56f268f132fe15dc9030414fd
edwinfmesa <edwinfmesa@hotmail.ccom>
2014-09-08 17:23:33 -0500

    POO and SWIG migration complete for pricing

--- actarium_apps/customers_services/templates/payment_summary.js
+++ actarium_apps/customers_services/templates/payment_summary.js
@@ -26,45 +26,58 @@ var PaymentSummary = function(){
 			"price_total_months": numberWithCommas(this.price_total_months),
 			"discount": numberWithCommas(this.discount),
 			"iva": numberWithCommas(this.total*0.16),
-			"total": numberWithCommas(this.total,
+			"total": numberWithCommas(this.total),
+			// "total_without_iva": numberWithCommas(this.total*0.84),
 		}
 		return values_with_comma
 	}
 
+	this.updateDiscount = function(){
+		discount_code = this.discount_code;
+		this_obj = this;
+		sendNewAjax("{% url 'core:get_discount_value' %}",
+			{"discount_code":discount_code},
+			function(data){
+				if (data.Error){
+            		console.log(data.Error);
+            		this_obj.discount = 0
+            	}
+            	else{
+            		this_obj.discount = data.discount_value
+            	}
+            	this_obj.total = this_obj.price_total_months - this_obj.discount;
+				
+				this_obj.loadHtmlData();
+				console.log(this_obj.valuesForTemplate())
+			},
+			{"method":"post"}
+		)
+	}
+
+
+
 	this.updatePricePerMonth = function(){
 		number_of_members = this.number_of_members
+		this_obj = this;
 		sendNewAjax(
 			"{% url 'core:get_total_price' %}",
 			{"quantity":number_of_members},
 			function(data){
 				if (data.Error){
-					console.log("Ha ocurrido un error intentando recibir los datos del servidor por AJAX")
+					console.log("Ha ocurrido un error intentando recibir los datos del servidor por AJAX",data.Error)
 				}
 				else{
-					this.price_per_month = data.quantity
+					this_obj.price_per_month = data.quantity
 				}
+				this_obj.price_total_months = this_obj.number_of_months*this_obj.price_per_month;
+				console.log(this_obj)
+				this_obj.updateDiscount();
 			},
 			{"method":"post"}
 		)
 	}
 
-	this.updateDiscount = function(){
-		discount_code = this.discount_code;
-		sendNewAjax("{% url 'core:get_discount_value' %}",
-			{"discount_code":discount_code},
-			function(){
-				if (data.Error){
-            		console.log(data.Error)
-            	}
-            	else{
-            		$('#discountValue').html(numberWithCommas(data.discount_value))
-            		setTotalPrice();
-            	}
-			},
-			{"method":"post"}
-		)
-	}
-
+	
 	this.loadHtmlData = function(){
 		tpl = $("#paymentSummaryTpl").html()
 		html_data = swig.render(tpl,{locals: this.valuesForTemplate() })
@@ -72,10 +85,15 @@ var PaymentSummary = function(){
 	}
 
 	this.updateWithForm = function(){
-		this.number_of_members = $('#id_number_of_members').val();
-    	this.number_of_months = $('#id_number_of_months').val();
+		number_of_members = $('#id_number_of_members').val();
+		console.log(number_of_members)
+		this.number_of_members = number_of_members;
+		number_of_months =$('#id_number_of_months').val();
+		console.log(number_of_months)
+    	this.number_of_months = number_of_months;
     	this.payment_method = $('#id_payment_method').val();
     	this.discount_code = $('#id_discount').val()
+    	// console.log(this.valuesForTemplate())
     }
 
     this.update = function(){
@@ -83,11 +101,6 @@ var PaymentSummary = function(){
 		this.updateWithForm();
 		//update object data
 		this.updatePricePerMonth();
-		this.price_total_months = this.number_of_months*this.price_per_month;
-		this.updateDiscount();
-		this.total = this.price_total_months - this.discount
-		//update html
-		this.loadHtmlData()
 	}
 }
 
