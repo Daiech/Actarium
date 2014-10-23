69d7684e0190ee8b531841393892cdfbf00dd364
edwinfmesa <edwinfmesa@hotmail.ccom>
2014-10-22 19:33:38 -0500

    include getGlobalVar on customers_services -> views_ajax

--- actarium_apps/customers_services/views_ajax.py
+++ actarium_apps/customers_services/views_ajax.py
@@ -16,11 +16,10 @@ def approve_order(request):
             if order_item:
                 status_approve = OrderStatus.objects.get(code="002")
                 status_defeated = OrderStatus.objects.get(code="006")
-                print status_approve
-                print status_defeated
+
                 
                 last_order = order_item.customer_service.get_order_active()
-
+                from apps.website.views import getGlobalVar
                 FREE_SERVICE = getGlobalVar("FREE_SERVICE")
                 if last_order.service.code==FREE_SERVICE:
 
@@ -38,23 +37,17 @@ def approve_order(request):
                     customer_service = order_item.customer_service
                     customer_service.quantity = new_order.order_quantity
                     number_of_periods = new_order.number_of_periods
-                    print "number_of_periods", number_of_periods
                     date_expiration = datetime.date.today() + datetime.timedelta(number_of_periods*30,0,0)
-                    print date_expiration
                     customer_service.date_expiration = date_expiration
                     customer_service.save()
 
                     message = {'is_done':True,'response':_("Modificado Correctamente")}
-                    print message
                 else:
                     message = {'Error': _(u"Opción no implementada, únicamente se puede actualizar la version trial" )}
-                    print message['Error']
             else:
-                message = {'Error': _( "No hay orden disponible" )}
-                print message['Error']
+                message = {'Error': _( "No hay orden disponible" )}                
     else:
         message = {'Error': _( u"No tiene permiso para realizar esta acción" )}
-        print message['Error']
         
     return HttpResponse(json.dumps(message), mimetype="application/json")
     
