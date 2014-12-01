9ef6248189b9d918e7e2a7b23a8a655e04880ee9
edwinfmesa <edwinfmesa@hotmail.ccom>
2014-12-01 12:53:20 -0500

    Starting with notifications module

--- apps/actions_log/models.py
+++ apps/actions_log/models.py
@@ -1,5 +1,6 @@
 #encoding:utf-8
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
 
@@ -19,3 +20,30 @@ class rel_user_action(models.Model):
     extra = models.TextField(blank=True)
     date_done = models.DateTimeField(auto_now=True)
     ip_address = models.CharField(max_length=100, verbose_name="IP_address")
+
+
+class PersonalNotification(models.Model):
+    image = models.CharField(max_length=200, verbose_name=_(u"Imagen"))
+    message = models.CharField(max_length=200, verbose_name=_(u"Mensaje"))
+    url = models.CharField(max_length=200, verbose_name=_(u"Url"))
+    user_generator = models.ForeignKey(User,  null=False, related_name='%(class)s_user_generator')
+    user_responsible = models.ForeignKey(User,  null=False, related_name='%(class)s_user_responsible')
+    type_notification = models.CharField(max_length=200, verbose_name=_(u"Tipo"))
+    viewed = models.BooleanField(default=True)
+
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+# class GroupalNotification(models.Model):
+#     image = models.CharField(max_length=200, verbose_name=_(u"Imagen"))
+#     message = models.CharField(max_length=200, verbose_name=_(u"Mensaje"))
+#     url = models.CharField(max_length=200, verbose_name=_(u"Url"))
+#     user_generator = models.ForeignKey(User,  null=False, related_name='%(class)s_user_generator')
+#     user_responsible = models.ForeignKey(User,  null=False, related_name='%(class)s_user_responsible')
+#     type_notification = models.CharField(max_length=200, verbose_name=_(u"Tipo"))
+#     viewed = models.BooleanField(default=True)
+
+#     is_active = models.BooleanField(default=True)
+#     created = models.DateTimeField(auto_now_add=True)
+#     modified = models.DateTimeField(auto_now=True)
