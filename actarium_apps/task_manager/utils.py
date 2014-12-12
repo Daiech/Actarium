dbc76147b5399e69cc0087da7833c6a6391d7894
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-12-12 05:02:03 -0500

    Fixed Task description as json

--- actarium_apps/task_manager/utils.py
+++ actarium_apps/task_manager/utils.py
@@ -9,7 +9,7 @@ def task_as_json(task_obj):
                 "title":task_obj.name,
                 "short_title":task_obj.name[0:title_max_length]+"..." if len(task_obj.name) >= title_max_length else task_obj.name,
                 "description":task_obj.description,
-                "short_description":task_obj.description[0:description_max_length]+"..." if len(task_obj.description) >= description_max_length else task_obj.description,
+                "short_description":task_obj.description[0:description_max_length]+"..." if task_obj.description and len(task_obj.description) >= description_max_length else task_obj.description,
                 "responsible_img":showgravatar(task_obj.responsible.email,25), 
                 "responsible":task_obj.responsible.first_name, 
                 "responsible_id":task_obj.responsible.id, 
